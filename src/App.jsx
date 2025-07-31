b99190024732e7f612f66e1b4254cae4c8acaec0
Amitabh Anmol Pain <ami05tabh@gmail.com>
2025-07-31 20:48:29 +0530

    Updated package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import About from './components/About'
-import Projects from './components/Projects'
-import Testimonails from './components/Testimonails'
-import Contact from './components/Contact'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Testimonails from '../components/Testimonails'
+import Contact from '../components/Contact'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Footer from './components/Footer'
-import Header from './components/header'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 
 
